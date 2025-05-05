146e423cca7d332ec025dda9ca9b9fe41c5eb7b0
Enda Bigarella <enda.bigarella@gmail.com>
2025-05-05 17:17:55 +0200

    Matplotlib font setup, serif, sizes.

--- gpr_aniso_trial.py
+++ gpr_aniso_trial.py
@@ -11,6 +11,13 @@ import matplotlib
 matplotlib.use('TkAgg')
 matplotlib.set_loglevel('critical')
 
+matplotlib.rcParams['font.family'] = 'serif'
+matplotlib.rcParams['xtick.labelsize'] = 14
+matplotlib.rcParams['ytick.labelsize'] = 14
+matplotlib.rcParams['axes.titlesize']  = 16
+matplotlib.rcParams['axes.labelsize']  = 16
+matplotlib.rcParams['legend.fontsize'] = 14
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
